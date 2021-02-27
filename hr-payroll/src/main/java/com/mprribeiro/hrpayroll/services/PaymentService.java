1a158d864d39b3a8487c2c0745d31158e153c2b3
Marcos Ribeiro <mprribeiro1902@gmail.com>
2021-02-27 20:58:17 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/mprribeiro/hrpayroll/services/PaymentService.java
@@ -0,0 +1,13 @@
+package com.mprribeiro.hrpayroll.services;
+
+import org.springframework.stereotype.Service;
+
+import com.mprribeiro.hrpayroll.entities.Payment;
+
+@Service
+public class PaymentService {
+
+	public Payment getPayment(Long workerId, Integer days) {
+		return new Payment("Bob", 200.0, days);
+	}
+}
