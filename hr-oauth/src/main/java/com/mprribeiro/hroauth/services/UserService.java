0eecb58062e417d7ff0504c5d6b7d79d8fe61e37
Marcos Ribeiro <mprribeiro1902@gmail.com>
2021-03-01 22:21:28 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/mprribeiro/hroauth/services/UserService.java
@@ -0,0 +1,27 @@
+package com.mprribeiro.hroauth.services;
+
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mprribeiro.hroauth.entities.User;
+import com.mprribeiro.hroauth.feignClients.UserFeignClient;
+
+@Service
+public class UserService {
+	
+	private static Logger logger = org.slf4j.LoggerFactory.getLogger(UserService.class);
+
+	@Autowired
+	private UserFeignClient userFeignClient;
+	
+	public User findByEmail(String email) {
+		User user = userFeignClient.findByEmail(email).getBody();
+		if (user == null) {
+			logger.error("Email not found: " + email);
+			throw new IllegalArgumentException("Email not found!");
+		} 
+
+		return user;
+	}
+}
